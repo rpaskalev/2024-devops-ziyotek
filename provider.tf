347343de6a9a3425d64ac8c7822b8831809d58f3
irynaglu <Iryna.Glushak@ziyotek.net>
2024-05-07 18:42:51 -0500

    made changes to files

--- provider.tf
+++ provider.tf
@@ -2,20 +2,20 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-    # required_providers {
-    #   aws = {
-    #     source = "hashicorp/aws
-    #     version = "~> 3.45"
-    #   }
-    # }
+# terraform {
+#     # required_providers {
+#     #   aws = {
+#     #     source = "hashicorp/aws
+#     #     version = "~> 3.45"
+#     #   }
+#     # }
 
-  backend "s3" {
-    bucket = "rady-bucket-1-0-0-0"
-    key    = "terraform-state" #a folder
-    region = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt = true
-  }
-}
+#   backend "s3" {
+#     bucket = "rady-bucket-1-0-0-0"
+#     key    = "terraform-state" #a folder
+#     region = "us-east-1"
+#     dynamodb_table = "terraform-lock"
+#     encrypt = true
+#   }
+# }
 
--- s3.tf
+++ s3.tf
@@ -1,26 +1,43 @@
-resource "aws_s3_bucket" "ziyotek_bucket" {
-  bucket        = "rady-devops-2-24-bucket-1"
+resource "aws_s3_bucket" "monopoly_bucket" {
+  bucket        = "iryna-devops-2-24-bucket-1"
   force_destroy = true
   tags = {
-    Name        = "My bucket"
-    Environment = "sbx"
+    Name        = "My_monopoly_bucket"
+    Owner = "Devops_Iryna"
   }
 }
 
-resource "aws_s3_bucket" "ziyotek_bucket_2" {
-  bucket        = "rady-devops-2-24-bucket-2"
-  force_destroy = true
-  tags = {
-    Name        = "My bucket"
-    Environment = "sbx"
+
+resource "aws_s3_bucket_versioning" "versioning_monopoly_bucket" {
+  bucket = aws_s3_bucket.monopoly_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket" "ziyotek_bucket_3" {
-  bucket = "rady-devops-2-24-bucket-3"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "sbx"
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_monopoly_bucket" {
+  bucket = aws_s3_bucket.monopoly_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+
+
+resource "aws_s3_bucket_acl" "acl_monopoly_bucket" {
+  bucket = aws_s3_bucket.monopoly_bucket.id
+  acl    = "public-read"
+}
+
+
+resource "aws_s3_bucket_object_lock_configuration" "object_lock_monopoly_bucket" {
+  bucket = aws_s3_bucket.monopoly_bucket.id
+  object_lock_enabled = "Enabled"
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
   }
 }
