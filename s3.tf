24787065b8520e0586fec96d8165ff1498f9ad5b
abrarr1998 <abrarr1998@gmail.com>
2024-05-07 19:29:15 -0400

    abrar's edit

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "ziyotek_bucket" {
-  bucket = "tsering-namgyal-bucket"
+  bucket = "abrar3289-bucket"
 
   tags = {
     Name        = "My bucket "
@@ -7,20 +7,15 @@ resource "aws_s3_bucket" "ziyotek_bucket" {
   }
 }
 
-//versioning 
+resource "aws_s3_bucket_acl" "example" {
 
-resource "aws_s3_bucket_versioning" "s3_versioning" {
   bucket = aws_s3_bucket.ziyotek_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
+  acl    = "public-read"
 }
 
-//Object lock for s3 
-
-resource "aws_s3_bucket_object_lock_configuration" "ziyotek_bucket" {
+resource "aws_s3_bucket_object_lock_configuration" "example432" {
   bucket = aws_s3_bucket.ziyotek_bucket.id
-
+  object_lock_enabled = "Enabled"
   rule {
     default_retention {
       mode = "COMPLIANCE"
@@ -30,52 +25,75 @@ resource "aws_s3_bucket_object_lock_configuration" "ziyotek_bucket" {
 }
 
 
-//encryptions on s3
+//versioning 
 
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
-  bucket = aws_s3_bucket.ziyotek_bucket.id
+# resource "aws_s3_bucket_versioning" "s3_versioning" {
+#   bucket = aws_s3_bucket.ziyotek_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-  rule {
-    apply_server_side_encryption_by_default {
-      #kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm = "AES256"
-      #sse_algorithm     = "aws:kms"
+# //Object lock for s3 
 
-    }
-    bucket_key_enabled = "true"
-  }
-}
+# resource "aws_s3_bucket_object_lock_configuration" "ziyotek_bucket" {
+#   bucket = aws_s3_bucket.ziyotek_bucket.id
 
-//acl
-resource "aws_s3_bucket_ownership_controls" "ziyotek_bucket" {
-  bucket = aws_s3_bucket.ziyotek_bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
+#   rule {
+#     default_retention {
+#       mode = "COMPLIANCE"
+#       days = 5
+#     }
+#   }
+# }
 
-resource "aws_s3_bucket_public_access_block" "ziyotek_bucket" {
-  bucket = aws_s3_bucket.ziyotek_bucket.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# //encryptions on s3
 
-data "aws_canonical_user_id" "current" {}
-resource "aws_s3_bucket_acl" "ziyotek_bucket" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.ziyotek_bucket,
-    #aws_s3_bucket_public_access_block.ziyotek_bucket,
-  ]
+# resource "aws_kms_key" "mykey" {
+#   description             = "This key is used to encrypt bucket objects"
+#   deletion_window_in_days = 10
+# }
+# resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
+#   bucket = aws_s3_bucket.ziyotek_bucket.id
 
-  bucket = aws_s3_bucket.ziyotek_bucket.id
-  acl    = "public-read"
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       #kms_master_key_id = aws_kms_key.mykey.arn
+#       sse_algorithm = "AES256"
+#       #sse_algorithm     = "aws:kms"
 
-}
+#     }
+#     bucket_key_enabled = "true"
+#   }
+# }
+
+# //acl
+# resource "aws_s3_bucket_ownership_controls" "ziyotek_bucket" {
+#   bucket = aws_s3_bucket.ziyotek_bucket.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
+
+# resource "aws_s3_bucket_public_access_block" "ziyotek_bucket" {
+#   bucket = aws_s3_bucket.ziyotek_bucket.id
+
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
+
+# data "aws_canonical_user_id" "current" {}
+# resource "aws_s3_bucket_acl" "ziyotek_bucket" {
+#   depends_on = [
+#     aws_s3_bucket_ownership_controls.ziyotek_bucket,
+#     #aws_s3_bucket_public_access_block.ziyotek_bucket,
+#   ]
+
+#   bucket = aws_s3_bucket.ziyotek_bucket.id
+#   acl    = "public-read"
+
+# }
 
