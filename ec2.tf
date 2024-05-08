394eeb7fa520644341acd2a0532c7dc3f7fc94a1
rpaskalev <rpaskalev@lomixtech.com>
2024-05-07 20:33:07 -0400

    adding ec2

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami = "ami-07caf09b362be10b8"
+
+  subnet_id                   = aws_subnet.ziyo_subnet_public.id
+
+  key_name      = "radostinpaskalev_key"
+  instance_type = "t2.micro"
+associate_public_ip_address = true
+  # Security group must be declared in the network_interface_id block if we are adding that parameter.
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+}
