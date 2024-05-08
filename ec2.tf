58169f0016a8678c6b456adf2c3ad7bb6d911e8a
rpaskalev <rpaskalev@lomixtech.com>
2024-05-07 20:39:15 -0400

    put together ec2 and SG

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 associate_public_ip_address = true
   # Security group must be declared in the network_interface_id block if we are adding that parameter.
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
 }
