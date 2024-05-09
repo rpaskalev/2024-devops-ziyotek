c84b08259bc4f5e1741ad196613bc6a7491d1ee6
tseringnam <grgtsering@gmail.com>
2024-05-09 00:20:54 -0400

    Ingress rule

--- security_group.tf
+++ security_group.tf
@@ -1,26 +1,45 @@
 resource "aws_security_group" "allow_tls" {
- name        = "allow_tls"
- description = "Allow TLS inbound traffic and all outbound traffic"
- vpc_id      = aws_vpc.ziyo_vpc.id
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.ziyo_vpc.id
 
-
- tags = {
-   Name = "allow_tls"
- }
+   ingress {
+    cidr_blocks   = ["0.0.0.0/0"]
+    from_port   = 22
+    protocol = "tcp"
+    to_port     = 22
+  }
+  ingress {
+    cidr_blocks   = ["0.0.0.0/0"]
+    from_port   = 5432
+    protocol= "tcp"
+    to_port     = 5432
+  }
+  tags = {
+    Name = "allow_tls"
+  }
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
- security_group_id = aws_security_group.allow_tls.id
- cidr_ipv4         = aws_vpc.ziyo_vpc.cidr_block
- from_port         = 0
- ip_protocol       = "-1"
- to_port           = 0
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   ingress {
+#     cidr_ipv4   = aws_vpc.ziyo_vpc.cidr_block
+#     from_port   = 22
+#     ip_protocol = "tcp"
+#     to_port     = 22
+#   }
+#   ingress {
+#     cidr_ipv4   = aws_vpc.ziyo_vpc.cidr_block
+#     from_port   = 5432
+#     ip_protocol= "tcp"
+#     to_port     = 5432
+#   }
+# }
 
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
- security_group_id = aws_security_group.allow_tls.id
- cidr_ipv4         = "0.0.0.0/0"
- ip_protocol       = "-1" # semantically equivalent to all ports
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
