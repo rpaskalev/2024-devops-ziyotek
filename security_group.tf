5599a3efdf29fc7c89b64a21c5deb399f88edbe6
Damir94 <abdurakhimov.da@gmail.com>
2024-05-09 16:22:09 -0400

    Modified files

--- security_group.tf
+++ security_group.tf
@@ -1,26 +1,39 @@
 resource "aws_security_group" "allow_tls" {
- name        = "allow_tls"
- description = "Allow TLS inbound traffic and all outbound traffic"
- vpc_id      = aws_vpc.ziyo_vpc.id
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.ziyo_vpc.id
 
 
- tags = {
-   Name = "allow_tls"
- }
+  # Ingress rule for port 5432 (PostgreSQL)
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic from any IP address 
+  }
+
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow SSH traffic from any IP address 
+  }
+
+  # Egress rules (outbound traffic)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"] # Allow traffic to any IP address (be cautious in production)
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
 
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
- security_group_id = aws_security_group.allow_tls.id
- cidr_ipv4         = aws_vpc.ziyo_vpc.cidr_block
- from_port         = 0
- ip_protocol       = "-1"
- to_port           = 0
-}
 
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
- security_group_id = aws_security_group.allow_tls.id
- cidr_ipv4         = "0.0.0.0/0"
- ip_protocol       = "-1" # semantically equivalent to all ports
-}
+
