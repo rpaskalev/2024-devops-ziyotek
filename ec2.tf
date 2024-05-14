531ebbd5d726cfe81231dadf30615fc5922ed95d
S-u-n-e <Eric.Sun@ziyotek.net>
2024-05-14 12:19:30 -0500

    Finished

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,22 @@
 resource "aws_instance" "web_server1" {
-  ami                         = "ami-07caf09b362be10b8"
+  ami                         = var.ami_id
   subnet_id                   = aws_subnet.ziyo_subnet_public.id
   vpc_security_group_ids      = [aws_security_group.ziyo_security_group.id]
-  associate_public_ip_address = true
-  instance_type               = "t2.micro"
+  associate_public_ip_address = var.associate_public
+  instance_type               = var.instance_type
   #key_name                    = "ferro-key.pem"
-
-
-  tags = {
-    Name = "web_server1"
-  }
+  monitoring = var.ec2_monitoring
+  //ebs_optimized = var.ebs_optimized
+  tags = var.ec2_tags
 }  
+//Cloudwatch Alarm
+# resource "aws_cloudwatch_metric_alarm" "web_monitor" {
+#   alarm_name = "terraform-test-foobar5"
+#   comparison_operator = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods = "CPUUtilization"
+#   namespace = "AWS/EC2"
+#   period = 120
+#   statistic = "Average"
+#   threshold = 80
+#   alarm_description = "This metric monitors ec2 cpu utilization"
+# }
--- s3.tf
+++ s3.tf
@@ -1,10 +1,7 @@
 resource "aws_s3_bucket" "ziyotek_bucket" {
-  bucket = "your-name-devops-bucket"
+  bucket = var.ziyotek_bucket
 
-  tags = {
-    Name        = "My bucket "
-    Environment = "Dev"
-  }
+  tags = var.bucket_tags
 }
 
 //versioning 
@@ -12,7 +9,7 @@ resource "aws_s3_bucket" "ziyotek_bucket" {
 resource "aws_s3_bucket_versioning" "s3_versioning" {
   bucket = aws_s3_bucket.ziyotek_bucket.id
   versioning_configuration {
-    status = "Enabled"
+    status = var.version_status
   }
 }
 
@@ -23,8 +20,8 @@ resource "aws_s3_bucket_object_lock_configuration" "ziyotek_bucket" {
 
   rule {
     default_retention {
-      mode = "COMPLIANCE"
-      days = 5
+      mode = var.bucket_object_lock_mode
+      days = var.bucket_object_lock_days
     }
   }
 }
@@ -37,13 +34,34 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encryption" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      sse_algorithm = var.encryption
     }
   }
 }
 
 resource "aws_s3_bucket_acl" "ziyotek_bucket" {
   bucket = aws_s3_bucket.ziyotek_bucket.id
-  acl    = "public-read"
+  acl    = var.acl_type
+
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.ziyotek_bucket.id
+
+  block_public_acls       = var.example_public_acls
+  block_public_policy     = var.example_public_policy
+  ignore_public_acls      = var.example_ignore_public_acls
+  restrict_public_buckets = var.example_restrict_public_buckets
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.ziyotek_bucket.id
+
+  rule {
+    object_ownership = var.obj_ownership
+  }
+}
