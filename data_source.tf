fae33fbb3aa8158f07f570dce75d9febfb12a609
irynaglu <Iryna.Glushak@ziyotek.net>
2024-05-16 21:27:03 -0500

    made some changes

--- data_source.tf
+++ data_source.tf
@@ -1,21 +1,21 @@
-variable "security_group_1_id" {
+variable "security_group_1" {
     type = string
     default = "sg-09367113e578a5c90"
 }
 
 data "aws_security_group" "sec_group_1" {
-  id = var.security_group_1_id
+  id = var.security_group_1
 }
 
-variable "security_group_2_id" {
+variable "security_group_2" {
     type = string
     default = "sg-0abdf8c6032554c55"
 }
 
 data "aws_security_group" "sec_group_2" {
-  id = var.security_group_2_id
+  id = var.security_group_2
 }
 
-output name {
-    value = [ data.aws_security_group.sec_group_1.arn , data.aws_security_group.sec_group_2.arn ]
+output "show_me_security_groups" {
+    value = [ data.aws_security_group.sec_group_2.arn , data.aws_security_group.sec_group_1.arn ]
 }
